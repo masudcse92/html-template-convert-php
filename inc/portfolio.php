d97397dc7dbfc481f65ba76795052287beba8bb4
masudcse92 <masudcse9292@gmail.com>
2024-09-27 20:26:14 +0600

    portfilio PHP file convert

---
+++ inc/portfolio.php
@@ -0,0 +1,126 @@
+    <!-- portfolio -->
+    <div id="portfolio" class="portfolio segments">
+        <div class="container">
+            <div class="box-content">
+                <div class="section-title">
+                    <h3>My Portfolio</h3>
+                </div>
+                <div class="portfolio-filter-menu">
+                    <ul>
+                        <li data-filter="all" class="active">
+                            <span>See All</span>
+                        </li>
+                        <li data-filter="1">
+                            <span>House Plants</span>
+                        </li>
+                        <li data-filter="2">
+                            <span>Flowers</span>
+                        </li>
+                        <li data-filter="3">
+                            <span>Photography</span>
+                        </li>
+                    </ul>
+                </div>
+
+                <div class="row no-gutters filtr-container">
+                    <div class="col-md-4 col-sm-12 col-xs-12 filtr-item" data-category="3">
+                        <div class="content-image">
+                            <a href="images/portfolio1.jpg" class="portfolio-popup">
+                                <img src="images/portfolio1.jpg" alt="">
+                                <div class="image-overlay"></div>
+                                <div class="portfolio-caption">
+                                    <div class="title">
+                                        <h4>Classic Minimalist Door</h4>
+                                    </div>
+                                    <div class="subtitle">
+                                        <span>Graphic Design</span>
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="col-md-4 col-sm-12 col-xs-12 filtr-item" data-category="1">
+                        <div class="content-image">
+                            <a href="images/portfolio2.jpg" class="portfolio-popup">
+                                <img src="images/portfolio2.jpg" alt="">
+                                <div class="image-overlay"></div>
+                                <div class="portfolio-caption">
+                                    <div class="title">
+                                        <h4>Green Succulent Plants</h4>
+                                    </div>
+                                    <div class="subtitle">
+                                        <span>Nature Plant</span>
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="col-md-4 col-sm-12 col-xs-12 filtr-item" data-category="2, 1">
+                        <div class="content-image">
+                            <a href="images/portfolio3.jpg" class="portfolio-popup">
+                                <img src="images/portfolio3.jpg" alt="">
+                                <div class="image-overlay"></div>
+                                <div class="portfolio-caption">
+                                    <div class="title">
+                                        <h4>Yellow Rose Pale Plant</h4>
+                                    </div>
+                                    <div class="subtitle">
+                                        <span>Flower Plant</span>
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="col-md-4 col-sm-12 col-xs-12 filtr-item" data-category="3">
+                        <div class="content-image">
+                            <a href="images/portfolio4.jpg" class="portfolio-popup">
+                                <img src="images/portfolio4.jpg" alt="">
+                                <div class="image-overlay"></div>
+                                <div class="portfolio-caption">
+                                    <div class="title">
+                                        <h4>Succulent Plant in Pot</h4>
+                                    </div>
+                                    <div class="subtitle">
+                                        <span>Plant House</span>
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="col-md-4 col-sm-12 col-xs-12 filtr-item" data-category="1, 2">
+                        <div class="content-image">
+                            <a href="images/portfolio5.jpg" class="portfolio-popup">
+                                <img src="images/portfolio5.jpg" alt="">
+                                <div class="image-overlay"></div>
+                                <div class="portfolio-caption">
+                                    <div class="title">
+                                        <h4>Photo of Green Cactus</h4>
+                                    </div>
+                                    <div class="subtitle">
+                                        <span>Plant House</span>
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="col-md-4 col-sm-12 col-xs-12 filtr-item" data-category="3, 1">
+                        <div class="content-image">
+                            <a href="images/portfolio6.jpg" class="portfolio-popup">
+                                <img src="images/portfolio6.jpg" alt="">
+                                <div class="image-overlay"></div>
+                                <div class="portfolio-caption">
+                                    <div class="title">
+                                        <h4>Two Feet of Two Ferns</h4>
+                                    </div>
+                                    <div class="subtitle">
+                                        <span>Graphic Design</span>
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- end portfolio -->
