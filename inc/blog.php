6ee9f5e8208bfb80ec1835c6726bdf88206d2fb4
masudcse92 <masudcse9292@gmail.com>
2024-09-27 20:28:11 +0600

    Blog PHP file convert

---
+++ inc/blog.php
@@ -0,0 +1,37 @@
+    <!-- blog -->
+    <div id="blog" class="blog segments">
+        <div class="container">
+            <div class="section-title">
+                <h3>My Blog</h3>
+            </div>
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="content">
+                        <div class="image">
+                            <img src="images/blog1.jpg" alt="">
+                        </div>
+                        <div class="blog-title">
+                            <h4><a href="#">Graphic Design and Photography Workshop This Year</a></h4>
+                            <div class="date">
+                                August 23, 2019 <i class="fas fa-circle"></i> <a href=""><span>GRAPHIC</span></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="content no-mb">
+                        <div class="image">
+                            <img src="images/blog2.jpg" alt="">
+                        </div>
+                        <div class="blog-title">
+                            <h4><a href="#">Creeping Plants for Minimalist Home Decorations</a></h4>
+                            <div class="date">
+                                August 23, 2019 <i class="fas fa-circle"></i> <a href=""><span>NATURE</span></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- end blog -->
--- index.php
+++ index.php
@@ -6,176 +6,20 @@ include "inc/about.php";
 
 include "inc/resume.php";
 
+include "inc/portfolio.php";
+
+include "inc/blog.php";
+
+
 ?>
 
 
 
 
 
-    <!-- portfolio -->
-    <div id="portfolio" class="portfolio segments">
-        <div class="container">
-            <div class="box-content">
-                <div class="section-title">
-                    <h3>My Portfolio</h3>
-                </div>
-                <div class="portfolio-filter-menu">
-                    <ul>
-                        <li data-filter="all" class="active">
-                            <span>See All</span>
-                        </li>
-                        <li data-filter="1">
-                            <span>House Plant</span>
-                        </li>
-                        <li data-filter="2">
-                            <span>Flowers</span>
-                        </li>
-                        <li data-filter="3">
-                            <span>Photography</span>
-                        </li>
-                    </ul>
-                </div>
 
-                <div class="row no-gutters filtr-container">
-                    <div class="col-md-4 col-sm-12 col-xs-12 filtr-item" data-category="3">
-                        <div class="content-image">
-                            <a href="images/portfolio1.jpg" class="portfolio-popup">
-                                <img src="images/portfolio1.jpg" alt="">
-                                <div class="image-overlay"></div>
-                                <div class="portfolio-caption">
-                                    <div class="title">
-                                        <h4>Classic Minimalist Door</h4>
-                                    </div>
-                                    <div class="subtitle">
-                                        <span>Graphic Design</span>
-                                    </div>
-                                </div>
-                            </a>
-                        </div>
-                    </div>
-                    <div class="col-md-4 col-sm-12 col-xs-12 filtr-item" data-category="1">
-                        <div class="content-image">
-                            <a href="images/portfolio2.jpg" class="portfolio-popup">
-                                <img src="images/portfolio2.jpg" alt="">
-                                <div class="image-overlay"></div>
-                                <div class="portfolio-caption">
-                                    <div class="title">
-                                        <h4>Green Succulent Plants</h4>
-                                    </div>
-                                    <div class="subtitle">
-                                        <span>Nature Plant</span>
-                                    </div>
-                                </div>
-                            </a>
-                        </div>
-                    </div>
-                    <div class="col-md-4 col-sm-12 col-xs-12 filtr-item" data-category="2, 1">
-                        <div class="content-image">
-                            <a href="images/portfolio3.jpg" class="portfolio-popup">
-                                <img src="images/portfolio3.jpg" alt="">
-                                <div class="image-overlay"></div>
-                                <div class="portfolio-caption">
-                                    <div class="title">
-                                        <h4>Yellow Rose Pale Plant</h4>
-                                    </div>
-                                    <div class="subtitle">
-                                        <span>Flower Plant</span>
-                                    </div>
-                                </div>
-                            </a>
-                        </div>
-                    </div>
-                    <div class="col-md-4 col-sm-12 col-xs-12 filtr-item" data-category="3">
-                        <div class="content-image">
-                            <a href="images/portfolio4.jpg" class="portfolio-popup">
-                                <img src="images/portfolio4.jpg" alt="">
-                                <div class="image-overlay"></div>
-                                <div class="portfolio-caption">
-                                    <div class="title">
-                                        <h4>Succulent Plant in Pot</h4>
-                                    </div>
-                                    <div class="subtitle">
-                                        <span>Plant House</span>
-                                    </div>
-                                </div>
-                            </a>
-                        </div>
-                    </div>
-                    <div class="col-md-4 col-sm-12 col-xs-12 filtr-item" data-category="1, 2">
-                        <div class="content-image">
-                            <a href="images/portfolio5.jpg" class="portfolio-popup">
-                                <img src="images/portfolio5.jpg" alt="">
-                                <div class="image-overlay"></div>
-                                <div class="portfolio-caption">
-                                    <div class="title">
-                                        <h4>Photo of Green Cactus</h4>
-                                    </div>
-                                    <div class="subtitle">
-                                        <span>Plant House</span>
-                                    </div>
-                                </div>
-                            </a>
-                        </div>
-                    </div>
-                    <div class="col-md-4 col-sm-12 col-xs-12 filtr-item" data-category="3, 1">
-                        <div class="content-image">
-                            <a href="images/portfolio6.jpg" class="portfolio-popup">
-                                <img src="images/portfolio6.jpg" alt="">
-                                <div class="image-overlay"></div>
-                                <div class="portfolio-caption">
-                                    <div class="title">
-                                        <h4>Two Feet of Two Ferns</h4>
-                                    </div>
-                                    <div class="subtitle">
-                                        <span>Graphic Design</span>
-                                    </div>
-                                </div>
-                            </a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <!-- end portfolio -->
 
-    <!-- blog -->
-    <div id="blog" class="blog segments">
-        <div class="container">
-            <div class="section-title">
-                <h3>My Blog</h3>
-            </div>
-            <div class="row">
-                <div class="col-md-6">
-                    <div class="content">
-                        <div class="image">
-                            <img src="images/blog1.jpg" alt="">
-                        </div>
-                        <div class="blog-title">
-                            <h4><a href="#">Graphic Design and Photography Workshop This Year</a></h4>
-                            <div class="date">
-                                August 23, 2019 <i class="fas fa-circle"></i> <a href=""><span>GRAPHIC</span></a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-6">
-                    <div class="content no-mb">
-                        <div class="image">
-                            <img src="images/blog2.jpg" alt="">
-                        </div>
-                        <div class="blog-title">
-                            <h4><a href="#">Creeping Plants for Minimalist Home Decorations</a></h4>
-                            <div class="date">
-                                August 23, 2019 <i class="fas fa-circle"></i> <a href=""><span>NATURE</span></a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <!-- end blog -->
+
 
     <!-- contact -->
     <div id="contact" class="contact segments">
