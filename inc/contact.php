45bb71c373655d7c1fddd25aae1e2c97e8bea1cd
masudcse92 <masudcse9292@gmail.com>
2024-09-27 20:29:57 +0600

    Contact us section PHP convert

---
+++ inc/contact.php
@@ -0,0 +1,70 @@
+    <!-- contact -->
+    <div id="contact" class="contact segments">
+        <div class="container">
+            <div class="box-content">
+                <div class="row">
+                    <div class="col-md-6 col-sm-12 col-xs-12">
+                        <div class="content-left">
+                            <div class="section-title section-title-left">
+                                <h3>Contact Me</h3>
+                            </div>
+                            <h2>Realize your dream with us</h2>
+                            <ul>
+                                <li>
+                                    <a href="#"><i class="fab fa-facebook-f"></i></a>
+                                </li>
+                                <li>
+                                    <a href="#"><i class="fab fa-twitter"></i></a>
+                                </li>
+                                <li>
+                                    <a href="#"><i class="fab fa-dribbble"></i></a>
+                                </li>
+                                <li>
+                                    <a href="#"><i class="fab fa-google"></i></a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-md-6 col-sm-12 col-xs-12">
+                        <div class="content-right">
+                            <form action="contact-form.php" class="contact-form" id="contact-form" method="post">
+                                <div class="row">
+                                    <div class="col">
+                                        <div id="first-name-field">
+                                            <input type="text" placeholder="First Name" class="form-control" name="form-name">
+                                        </div>
+                                    </div>
+                                    <div class="col">
+                                        <div id="last-name-field">
+                                            <input type="text" placeholder="Last Name" class="form-control" name="form-name">
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col">
+                                        <div id="email-field">
+                                            <input type="email" placeholder="Email Address" class="form-control" name="form-email">
+                                        </div>
+                                    </div>
+                                    <div class="col">
+                                        <div id="subject-field">
+                                            <input type="text" placeholder="Subject" class="form-control" name="form-subject">
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col">
+                                        <div id="message-field">
+                                            <textarea cols="30" rows="5" class="form-control" id="form-message" name="form-message" placeholder="Message"></textarea>
+                                        </div>
+                                    </div>
+                                </div>
+                                <button class="button" type="submit" id="submit" name="submit">Send Message</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- end contact -->
